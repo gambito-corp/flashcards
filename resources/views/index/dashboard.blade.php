8191c86bd6fc4552b428d85aea813080efa24872
Kenji <antoniosuaf27@gmail.com>
2025-03-05 17:26:58 -0500

    Correciones texto y se coloco sticky el header

--- resources/views/index/dashboard.blade.php
+++ resources/views/index/dashboard.blade.php
@@ -1,15 +1,15 @@
 <x-main-layout title="hola">
-    <div class=" bg-white border-b border-gray-200  container-ask box-dashboard relative overflow-hidden max-sm:m-5">
+    <div class=" bg-white border-b border-gray-200  container-ask box-dashboard relative overflow-hidden max-sm:m-5 shadow-md">
         <div class="">
 
 
             <div class="mb-4 bg-[radial-gradient(circle, rgb(2,85,91), rgb(1,25,27))]">
-                <div class="">
-                    <div class="">
+                <div class="grid grid-cols-2">
+                    <div class="span-2">
                         <img class="absolute right-0 bottom-0 mix-blend-luminosity hidden lg:block" src="/dashboard-img.png" />
                         <h2 class="mb-3 text-yellow-500 primary-color title-ask-container color-white">Obtén acceso total</h2>
                         <hr>
-                        <p class="pb-3"> Lleva tu preparación al siguiente nivel con todas las funcionalidades de ENAM.pe.</p>
+                        <p class="pb-3"> Lleva tu preparación al siguiente nivel con todas las funcionalidades de medbystudents.</p>
                         <a href="{{route('planes')}}" class="mt-3 inline-block bg-yellow-500 hover:bg-yellow-600 text-white font-semibold py-2 px-4 rounded boton-success-m">
                             Más información<img src="https://medbystudents.com/app-banqueo/wp-content/uploads/2025/03/arrow-mbs.svg"/>
                         </a>
