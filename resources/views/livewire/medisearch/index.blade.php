1116dabdfe8f8894fc9c5d45524a3af4e9bd67a8
Gambito Code/ <asesor.pedro@gmail.com>
2025-05-11 00:56:30 +0200

    Corrige renderizado de autores en medisearch
    Se reemplaza {{$autores}} por {!! $autores !!} para permitir la interpretación adecuada de HTML en los nombres de autores. Esto garantiza un despliegue correcto del contenido dinámico.

--- resources/views/livewire/medisearch/index.blade.php
+++ resources/views/livewire/medisearch/index.blade.php
@@ -330,7 +330,7 @@ bg-white shadow-lg w-[90%] sm:w-1/5 h-[calc(100vh_-_75px)]">
                                                            class="block">
                                                             <h4 class="font-bold text-[#195b81] text-sm">{{$titulo}}</h4>
                                                             <div class="mt-1 text-xs text-gray-500">
-                                                                {{$autores}}
+                                                                {!! $autores !!}
 
                                                                 @if(!empty($article['fecha'] ?? ''))
                                                                     ·     {{$fecha}}
