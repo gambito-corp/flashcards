e9471e7932be6f0478f5116b1d9f1a5543349ac8
Antoniogibux <antoniosuaf27@gmail.com>
2025-05-14 18:01:01 -0500

    examen ia

--- resources/views/livewire/exams/realice-ia.blade.php
+++ resources/views/livewire/exams/realice-ia.blade.php
@@ -4,27 +4,28 @@
     </script>
 @else
     <div x-data="examComponent()" x-init="init()" class="container mx-auto p-4 relative">
-        <div class="max-w-3xl mx-auto bg-white shadow-lg rounded-lg overflow-hidden">
+        <div class="max-w-7xl mx-auto  overflow-hidden">
             <!-- Encabezado -->
-            <div class="bg-blue-500 text-white px-6 py-4 flex justify-between items-center">
-                <h4 class="text-xl font-semibold" x-text="examSubmitted ? examTitle : 'Realizar Examen'"></h4>
-                <div class="text-lg font-semibold" x-show="!examSubmitted">
+            <div class="bg-[#5b8080] p-[30px] text-white flex justify-between items-center rounded-t-[20px]">
+                <h4 class="text-[17px] md:text-xl font-semibold" x-text="examSubmitted ? examTitle : 'Realizar Examen'"></h4>
+                <div class="text-[17px] md:text-xl font-semibold" x-show="!examSubmitted">
                     Tiempo restante: <span x-text="formattedTime"></span>
                 </div>
             </div>
 
             <!-- Preguntas/Paginación -->
+             <div class="p-[40px] bg-white rounded-b-[20px]">
             <div>
                 <template x-for="(question, index) in paginatedQuestions" :key="question.id">
                     <div class="mb-6">
-                        <h5 class="flex items-center text-lg font-semibold">
-                            <span class="inline-block bg-gray-200 text-gray-500 rounded-full px-3 py-1 mr-3">
-                                <span x-text="(currentPage - 1) * questionsPerPage + index + 1"></span>
+                        <h5 class="flex items-center flex-wrap text-lg font-semibold">
+                            <span class="inline-block  text-gray-500 rounded-full px-3 py-1 mr-3">
+                                <span x-text="(currentPage - 1) * questionsPerPage + index + 1" class=" w-[35px] h-[35px] flex justify-center items-center text-white bg-[var(--accent-color)] rounded-full"></span>
                             </span>
-                            <span x-text="question.content"></span>
+                            <span x-text="question.content" class="text-[17px] md:text-[18px] font-medium rounded-full"></span>
                         </h5>
                         <hr>
-                        <ul class="mt-2 space-y-2">
+                        <ul class="mt-2 space-y-2 flex flex-wrap md:gap-[20px] gap-[15px]">
                             <template x-for="option in question.options" :key="option.id">
                                 <li class="p-3 border rounded cursor-pointer"
                                     :class="getOptionClass(question.id, option.id)"
@@ -39,9 +40,9 @@
                             !selectedAnswers[question.id] ||
                             !correctAnswers.includes(selectedAnswers[question.id])
                         )">
-                            <div class="mt-2">
+                            <div class="mt-4">
                                 <strong class="text-red-500">*</strong>
-                                <span x-text="question.explanation"></span>
+                                <span class="text-[15px]" x-text="question.explanation"></span>
                                 <strong class="text-red-500">*</strong>
                             </div>
                         </template>
@@ -49,14 +50,14 @@
                 </template>
 
                 <!-- Paginación -->
-                <div class="flex justify-between items-center mt-6">
-                    <div>
+                <div class="flex justify-between items-center mt-6  flex-wrap mb-8">
+                    <div class="flex gap-5">
                         <button @click="prevPage()" :disabled="currentPage === 1"
-                                class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded disabled:opacity-50">
+                                class="bg-[#5b8080] text-white px-[35px] py-[10px] text-[14px] md:text-[16px] font-semibold rounded rounded disabled:opacity-50">
                             Anterior
                         </button>
                         <button @click="nextPage()" :disabled="currentPage === totalPages"
-                                class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded disabled:opacity-50">
+                                class="bg-[var(--primary-color)] text-white px-[35px] py-[10px] text-[14px] md:text-[16px] font-semibold rounded disabled:opacity-50">
                             Siguiente
                         </button>
                     </div>
@@ -64,7 +65,7 @@
                     <div class="flex flex-wrap gap-2 mt-4">
                         <template x-for="(question, index) in questions" :key="question.id">
                             <div @click="currentPage = index + 1"
-                                 class="w-10 h-10 flex items-center justify-center border cursor-pointer rounded"
+                                 class="w-10 h-10 flex items-center justify-center border cursor-pointer rounded border-[#f1f1f1]"
                                  :class="getSquareClass(question)">
                                 <span x-text="index + 1"></span>
                             </div>
@@ -76,21 +77,21 @@
                     Página <span x-text="currentPage"></span> de <span x-text="totalPages"></span>
                 </div>
             </div>
-
+</div>
             <!-- Footer -->
-            <div class="px-6 py-4 bg-gray-100 text-right">
+            <div class="px-6 py-4 text-right">
                 <template x-if="!examSubmitted">
                     <button type="button"
-                            class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded"
+                            class="bg-[#157b80] py-[10px] px-[35px] rounded-[8px] text-white font-semibold"
                             @click="submitExam()">
                         Enviar Examen
                     </button>
                 </template>
                 <template x-if="examSubmitted">
-                    <div>
+                    <div class="flex justify-between">
                         <p class="mt-4 font-semibold text-lg">Puntuación: <span x-text="score"></span>/100</p>
                         <a href="{{ route('dashboard') }}"
-                           class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded inline-block mt-4">
+                           class="bg-[#195b81] py-[10px] px-[35px] rounded-[8px] text-white rounded font-semibold">
                             Ir al Home
                         </a>
                     </div>
@@ -99,8 +100,8 @@
         </div>
 
         <!-- Flotante de tiempo -->
-        <div x-show="!examSubmitted" class="fixed bottom-4 right-4 bg-white shadow-lg rounded p-4" x-cloak>
-            <div class="text-base font-bold mb-1">Tiempo restante</div>
+        <div x-show="!examSubmitted" class="flex justify-center items-center gap-[30px] md:flex-row w-full md:w-auto md:top-[88px] top-auto bottom-0 md:bottom-auto right-0 md:right-[20px] bg-[var(--accent-color)] md:rounded-[10px] rounded-[0px] text-white text-center p-[20px] fixed" x-cloak>
+            <div class="text-base font-bold md:mb-1">Tiempo restante</div>
             <div class="text-2xl font-extrabold" x-text="formattedTime"></div>
         </div>
     </div>
@@ -206,11 +207,11 @@
                 getOptionClass(questionId, optionId) {
                     if (!this.examSubmitted) {
                         return this.selectedAnswers[questionId] === optionId
-                            ? "bg-blue-500 text-white"
+                            ? "bg-[#157b80] text-white"
                             : "bg-white";
                     } else {
                         if (this.correctAnswers.includes(optionId)) {
-                            return "bg-green-500 text-white";
+                            return "bg-[#157b80] text-white";
                         } else if (this.selectedAnswers[questionId] === optionId) {
                             return "bg-red-500 text-white";
                         } else {
@@ -222,17 +223,17 @@
                 getSquareClass(question) {
                     if (!this.examSubmitted) {
                         if (this.selectedAnswers[question.id]) {
-                            return "bg-blue-500 text-white border-blue-500";
+                            return "bg-[#157b80] text-white border-blue-500";
                         } else {
                             return "bg-white text-gray-800 border-gray-300";
                         }
                     } else {
                         const answer = this.selectedAnswers[question.id];
                         if (!answer) {
-                            return "bg-gray-400 text-gray-800 border-gray-400";
+                            return "bg-[#f1f1f1] text-gray-800 border-gray-400";
                         }
                         if (this.correctAnswers.includes(answer)) {
-                            return "bg-green-500 text-white border-green-500";
+                            return "bg-[#157b80] text-white border-green-500";
                         } else {
                             return "bg-red-500 text-white border-red-500";
                         }
@@ -240,5 +241,6 @@
                 }
             };
         }
+        
     </script>
 @endpush
