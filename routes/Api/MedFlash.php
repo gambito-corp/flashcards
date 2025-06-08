731e3d0c3d1719b76daa66048d94fd963608c4f9
Gambito Code/ <asesor.pedro@gmail.com>
2025-06-08 03:20:46 +0200

    Refactor MedFlash routes for clarity and organization; add AI generation route

--- routes/Api/MedFlash.php
+++ routes/Api/MedFlash.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+// Clave: api/med-flash
 
+/*
 //Flashcards
 Route::get('', 'index');
 Route::get('{id}', 'show')->where('id', '[0-9]+');
@@ -16,3 +17,21 @@ Route::get('ai-generate', 'generateAI');
 // Ruta de Inicio del Juego
 Route::post('start-game', 'setGame');
 Route::get('game', 'getGame');
+*/
+
+/*
+//Flashcards
+Route::get('/', 'index');
+Route::get('/{id}', 'show')->where('id', '[0-9]+');
+Route::post('/', 'store');
+Route::put('/{id}', 'update')->where('id', '[0-9]+');
+Route::delete('/{id}', 'destroy')->where('id', '[0-9]+');
+
+Route::get('/categories', 'categoryIndex');
+Route::post('/category', 'categoryStore');
+// Nueva ruta para IA
+Route::get('/ai-generate', 'generateAI');
+// Ruta de Inicio del Juego
+Route::post('/start-game', 'setGame');
+Route::get('/game', 'getGame');
+*/
